ef5c2721ef88763ed60b4f1220f1f0529f6e9a50
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2024-12-26 13:49:46 -0600

    Create main.java

---
+++ EclipseProjects/main.java
@@ -0,0 +1,25 @@
+package first_java_project2024;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); // first Java-applc hello world
+		// primitive data types = not changeable  
+		int first = 1;
+		System.out.println(first);
+		boolean grade = true;
+		char character = 'a';
+		System.out.println(grade);
+		// not primitive data type below 
+		String str = "67a#^go*";
+		int x = 8;
+		double g = 59;
+		double u = g / x;
+		System.out.println(u);
+		// exponents (must be in form of double)
+		double d = Math.pow(x, u);
+		System.out.println(d);
+		}
+
+}
